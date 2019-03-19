306d8fdb39cabe0170a30df4707c6542b04878b8
michaelfbagheri <mike.f.bagheri@gmail.com>
2019-03-19 19:49:18 -0400

    uploading the latest

--- client/src/Pages/Landing/index.js
+++ client/src/Pages/Landing/index.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+import {Modal, Card, Button} from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+const StyleDiv = {
+  border: 'solid 2px black'
+};
 
 class Landing extends Component {
 
+
   render() {
     return (
-      <div>
-                landing
-      </div>
+       <Grid container xs={12} m={2}> 
+       <Grid item>
+       <div steyle={StyleDiv}>test</div>
+       </Grid>
+          </Grid>
+      
     );
   }
 
